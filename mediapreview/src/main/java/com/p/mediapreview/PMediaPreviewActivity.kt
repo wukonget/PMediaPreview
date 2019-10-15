cfdc1d4940a0144237e32a920ea5a28c480019b8
pengchong <505261524@qq.com>
2019-10-15 09:46:15 +0800

    init

---
+++ mediapreview/src/main/java/com/p/mediapreview/PMediaPreviewActivity.kt
@@ -0,0 +1,13 @@
+package com.p.mediapreview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PMediaPreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p_preview)
+    }
+
+}
